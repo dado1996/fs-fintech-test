e12c07dee0ffa52ee9405b67d5347fe1c3cd7f25
itrm-team <santiago@viio.me>
2025-04-15 21:23:26 -0500

    hotfixes for backend and frontend build

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
