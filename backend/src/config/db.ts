22e505452dc48231b9eb12fd1acf9283701ed46a
juannpz <juan.pucheta@viio.me>
2025-04-16 00:39:28 -0300

    hotfix: make server work

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,16 +1,14 @@
 import { Sequelize } from 'sequelize';
-import * as dotenv from 'dotenv';
+import 'dotenv/config';
 import { initUser } from '../models/user';
-
-dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432', 10),
-  database: process.env.DB_NAME || 'finsim',
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'finsim',
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
 });
 
 export const initializeDatabase = async () => {
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ import * as admin from 'firebase-admin';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Initialize Firebase Admin SDK
 admin.initializeApp();
