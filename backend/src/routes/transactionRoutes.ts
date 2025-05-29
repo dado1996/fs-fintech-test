35e0f20485d78ef99fa6dff55740ee5c070fea32
Diego Delgado <diego.960705@gmail.com>
2025-05-29 15:57:18 -0500

    ✨ Scenario4: Add transaction service

--- backend/src/routes/transactionRoutes.ts
+++ backend/src/routes/transactionRoutes.ts
@@ -2,7 +2,10 @@ import express from "express";
 import authenticate from "../middleware/authMiddleware";
 import * as transactionController from "../controllers/transactionController";
 import { validate } from "../middleware/validateMiddleware";
-import { depositSchema } from "../validations/transactionValidation";
+import {
+  depositSchema,
+  transferSchema,
+} from "../validations/transactionValidation";
 
 const router = express.Router();
 
@@ -11,6 +14,12 @@ router.post(
   authenticate,
   validate(depositSchema, "body"),
   transactionController.deposit
+);
+router.post(
+  "/transfer",
+  authenticate,
+  validate(transferSchema, "body"),
+  transactionController.transfer
 );
 
 export default router;
